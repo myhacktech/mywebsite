b878636e89fff743f6776fbfea7747146c68176b
myhacktech <86517958+myhacktech@users.noreply.github.com>
2021-07-19 11:15:11 +0530

    Initial commit

---
+++ file2.php
@@ -0,0 +1,7 @@
+<html>
+<body bgcolor=cyan>
+<?php
+echo "<center><h1> I AM PERFECTLY ALRIGHT!!!!! ";
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+<body bgcolor=orange>
+<?php
+echo "<center ><a href='file2.php'>HELLO ,HOW ARE YOU??? ";
+?>
+</body>
+</html>
